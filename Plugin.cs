286d77446e82a32a5b9d2ee82323edb6bdd77ebd
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-07-16 00:35:02 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿namespace UKRestarterTest;
+
+using BepInEx;
+using System;
+using UnityEngine;
+
+[BepInPlugin("Bryan_-000-.UKRestarterTest", "UKRestarterTest", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // this is just to make OpenURL run as late as possible
+            // since if this instance is still alive when the next one starts loading, shit breaks
+            Application.quitting += () =>
+            {
+                // idk why but env variables are passed, and doorstop sets and init variable for whatever reason
+                // so i reset it since if you dont doorstop just skips loading bepinex :P
+                Environment.SetEnvironmentVariable("DOORSTOP_INITIALIZED", null);
+                Application.OpenURL(Paths.ExecutablePath);
+            };
+
+            Application.Quit();
+        }
+    }
+}
