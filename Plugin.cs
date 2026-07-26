7b954464e850ed626e0b26e159925a70e2f803f6
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-07-26 05:04:38 +0100

    DOORSTOP_DISABLE and some temp debugging

--- Plugin.cs
+++ Plugin.cs
@@ -2,11 +2,17 @@
 
 using BepInEx;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [BepInPlugin("Bryan_-000-.UKRestarterTest", "UKRestarterTest", "1.0.0")]
 public class Plugin : BaseUnityPlugin
 {
+    public void Start() =>
+        LogEnvVars();
+
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
@@ -15,13 +21,35 @@ public class Plugin : BaseUnityPlugin
             // since if this instance is still alive when the next one starts loading, shit breaks
             Application.quitting += () =>
             {
-                // idk why but env variables are passed, and doorstop sets some init variable for whatever reason
-                // so i reset it since if you dont doorstop just skips loading bepinex :P
+                LogEnvVars();
+                // idk why but env variables are passed, and doorstop sets some to prevent like loading twice or smt?? idk
+                // so i reset it since if you dont, doorstop just skips loading bepinex, and thus, all your mods :P
                 Environment.SetEnvironmentVariable("DOORSTOP_INITIALIZED", null);
+                Environment.SetEnvironmentVariable("DOORSTOP_DISABLE", null);
+
                 Application.OpenURL(Paths.ExecutablePath);
             };
 
             Application.Quit();
         }
+    }
+
+    public void LogEnvVars()
+    {
+        IEnumerable<(string, string)> vars = GrabEnvVars().OrderBy(var => var.var);
+        Logger.LogInfo("Enviroment Variables:\n\n" +
+            string.Join("\n\n",
+                vars.Select(var =>
+                    $"\"{var.Item1}\": \"{var.Item2}\""
+                )
+            )
+        );
+    }
+
+    public IEnumerable<(string var, string val)> GrabEnvVars()
+    {
+        IDictionaryEnumerator idict = Environment.GetEnvironmentVariables().GetEnumerator();
+        while (idict.MoveNext())
+            yield return (idict.Key.ToString(), idict.Value.ToString());
     }
 }
